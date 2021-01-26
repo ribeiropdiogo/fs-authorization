bc3da88207cb268f1f7418d088b57e2f5aeb8330
Diogo Ribeiro <ribeiropdiogo@gmail.com>
2021-01-26 22:54:12 +0000

    Input validation

--- auth-server/routes/index.js
+++ auth-server/routes/index.js
@@ -4,6 +4,7 @@ const Access = require('../controllers/access')
 const nodemailer = require("nodemailer");
 var config = require('../config');
 var CodeGenerator = require('node-code-generator');
+const { validate } = require('../models/acess');
 
 var generator = new CodeGenerator();
 var pattern = 'SSI*+';
@@ -32,30 +33,38 @@ router.get('/access', function(req, res, next) {
 
 router.post('/access', function(req, res, next) {
 
+  if(/^[a-zA-Z]+$/.test(req.body.owner) && /^[a-zA-Z]+$/.test(req.body.user) && /^[a-zA-Z]+$/.test(req.body.operation) && /^[a-zA-Z-_()\d]+\.[a-zA-Z]+$/.test(req.body.target)){
+    if(req.body.owner.length <= 32 && req.body.user.length <= 32 && req.body.operation.length <= 32 && req.body.target.length <= 255){
 
-  req.body.code = codes[Math.floor(Math.random() * 10000)];
-  var consult_code = codes[Math.floor(Math.random() * 10000)];
-  req.body.consult_code = consult_code;
-  req.body.authorized = false;
-  
-  var mailOptions = {
-    from: 'supermegasafe@gmail.com',
-    to: config.users.get(req.body.owner).email,
-    subject: 'Authorization Required in Filesystem',
-    text: 'The user ' + req.body.user + ' has requested access to ' + req.body.target + ' in order to ' + req.body.operation + '. If you wish to authorize this action use the following code: ' + req.body.code
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
+    req.body.code = codes[Math.floor(Math.random() * 10000)];
+    var consult_code = codes[Math.floor(Math.random() * 10000)];
+    req.body.consult_code = consult_code;
+    req.body.authorized = false;
+    
+    var mailOptions = {
+      from: 'supermegasafe@gmail.com',
+      to: config.users.get(req.body.owner).email,
+      subject: 'Authorization Required in Filesystem',
+      text: 'The user ' + req.body.user + ' has requested access to ' + req.body.target + ' in order to ' + req.body.operation + '. If you wish to authorize this action use the following code: ' + req.body.code
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    }); 
+
+    Access.insert(req.body)
+      .then(d => res.status(200).send(consult_code))
+      .catch(e => res.status(500).jsonp({error: e}))
     } else {
-      console.log('Email sent: ' + info.response);
+      res.status(406).send();
     }
-  }); 
-
-  Access.insert(req.body)
-    .then(d => res.status(200).send(consult_code))
-    .catch(e => res.status(500).jsonp({error: e}))
+  } else {
+    res.status(406).send();
+  }
 });
 
 router.get('/access/:id', function(req, res, next) {
@@ -69,8 +78,5 @@ router.post('/authorize', function(req, res, next) {
     .then(d => res.status(200).redirect('/'))
     .catch(e => res.status(500).jsonp({error: e}))
 });
-
-
-
 
 module.exports = router;
