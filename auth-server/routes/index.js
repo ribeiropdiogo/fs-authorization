7901479119a7a6497866b0e3ae671db78aa3049a
Diogo Ribeiro <ribeiropdiogo@gmail.com>
2021-01-27 16:02:59 +0000

    bugfixes

--- auth-server/routes/index.js
+++ auth-server/routes/index.js
@@ -20,45 +20,49 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('authorization', { title: 'File System Authorization' });
 });
+
+/*
+Rota apenas para efeitos de testes/consulta da bd. Não pode ser usada em produção.
 
 router.get('/access', function(req, res, next) {
   Access.list()
     .then(d => res.status(200).jsonp(d) )
     .catch(e => res.status(500).jsonp({error: e}))
 });
+*/
 
 router.post('/access', function(req, res, next) {
 
-  if(/^[a-zA-Z]+$/.test(req.body.owner) && /^[a-zA-Z]+$/.test(req.body.user) && /^[a-zA-Z]+$/.test(req.body.operation) && /^[a-zA-Z-_()\d]+\.[a-zA-Z]+$/.test(req.body.target)){
+  if(/^[a-zA-Z]+$/.test(req.body.owner) && /^[a-zA-Z]+$/.test(req.body.user) && /^[a-zA-Z]+$/.test(req.body.operation) && /^[a-zA-Z-_()\d~/\\]+\.[a-zA-Z]+$/.test(req.body.target)){
     if(req.body.owner.length <= 32 && req.body.user.length <= 32 && req.body.operation.length <= 32 && req.body.target.length <= 255){
+      
+      req.body.code = codes[Math.floor(Math.random() * 10000)];
+      var consult_code = codes[Math.floor(Math.random() * 10000)];
+      req.body.consult_code = consult_code;
+      req.body.authorized = false;
+      
+      var mailOptions = {
+        from: 'supermegasafe@gmail.com',
+        to: config.users.get(req.body.owner).email,
+        subject: 'Authorization Required in Filesystem',
+        text: 'The user ' + req.body.user + ' has requested access to ' + req.body.target + ' in order to ' + req.body.operation + '. If you wish to authorize this action use the following code: ' + req.body.code
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      }); 
 
-    req.body.code = codes[Math.floor(Math.random() * 10000)];
-    var consult_code = codes[Math.floor(Math.random() * 10000)];
-    req.body.consult_code = consult_code;
-    req.body.authorized = false;
-    
-    var mailOptions = {
-      from: 'supermegasafe@gmail.com',
-      to: config.users.get(req.body.owner).email,
-      subject: 'Authorization Required in Filesystem',
-      text: 'The user ' + req.body.user + ' has requested access to ' + req.body.target + ' in order to ' + req.body.operation + '. If you wish to authorize this action use the following code: ' + req.body.code
-    };
-    
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    }); 
+      Access.insert(req.body)
+        .then(d => res.status(200).send(consult_code))
+        .catch(e => res.status(500).jsonp({error: e}));
 
-    Access.insert(req.body)
-      .then(d => res.status(200).send(consult_code))
-      .catch(e => res.status(500).jsonp({error: e}))
     } else {
       res.status(406).send();
     }
